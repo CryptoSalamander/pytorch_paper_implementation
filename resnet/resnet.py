6b0721fac9cbd528f2aab40e0e10d97a76f8ee86
HYUNSOO HA <dhy03196@naver.com>
2021-10-14 02:24:03 +0900

    [MOD] Add BottleNeck Block for Deeper ResNet

--- resnet/resnet.py
+++ resnet/resnet.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 # Residual Block 구조 정의
 class BasicBlock(nn.Module):
+    mul = 1
     def __init__(self, in_planes, out_planes, stride=1):
         super(BasicBlock, self).__init__()
         
@@ -35,6 +36,45 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
     
+class BottleNeck(nn.Module):
+    mul = 4
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BottleNeck, self).__init__()
+        
+        #첫 Convolution은 너비와 높이 downsampling
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        
+        self.conv3 = nn.Conv2d(out_planes, out_planes*self.mul, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes*self.mul)
+        
+        self.shortcut = nn.Sequential()
+        
+        if stride != 1 or in_planes != out_planes*self.mul:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes*self.mul, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes*self.mul)
+            )
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+        
+        
+        
+    
     
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
@@ -52,7 +92,7 @@ class ResNet(nn.Module):
         self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.linear = nn.Linear(512, num_classes)
+        self.linear = nn.Linear(512 * block.mul, num_classes)
         
     def make_layer(self, block, out_planes, num_blocks, stride):
         # layer 앞부분에서만 크기를 절반으로 줄이므로, 아래와 같은 구조
@@ -60,7 +100,7 @@ class ResNet(nn.Module):
         layers = []
         for i in range(num_blocks):
             layers.append(block(self.in_planes, out_planes, strides[i]))
-            self.in_planes = out_planes
+            self.in_planes = block.mul * out_planes
         return nn.Sequential(*layers)
     
     def forward(self, x):
@@ -80,3 +120,15 @@ class ResNet(nn.Module):
     
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+def ResNet50():
+    return ResNet(BottleNeck, [3, 4, 6, 3])
+
+def ResNet101():
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+def ResNet152():
+    return ResNet(BottleNeck, [3, 8, 36, 3])
