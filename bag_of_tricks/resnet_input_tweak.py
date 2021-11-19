ec6fe71a3f03de4e0ad870b3e7684f98af6987b8
HYUNSOO HA <dhy03196@naver.com>
2021-11-20 07:52:56 +0900

    [ADD] ResNet with Tweak Input Layer

---
+++ bag_of_tricks/resnet_input_tweak.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Residual Block 구조 정의
+class BasicBlock(nn.Module):
+    mul = 1
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BasicBlock, self).__init__()
+        
+        # stride를 통해 너비와 높이 조정
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        
+        # stride = 1, padding = 1이므로, 너비와 높이는 항시 유지됨
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        
+        # x를 그대로 더해주기 위함
+        self.shortcut = nn.Sequential()
+        
+        # 만약 size가 안맞아 합연산이 불가하다면, 연산 가능하도록 모양을 맞춰줌
+        if stride != 1: # x와 
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes)
+            )
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += self.shortcut(x) # 필요에 따라 layer를 Skip
+        out = F.relu(out)
+        return out
+
+class BottleNeck(nn.Module):
+    mul = 4
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BottleNeck, self).__init__()
+        
+        #첫 Convolution은 너비와 높이 downsampling
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        
+        self.conv3 = nn.Conv2d(out_planes, out_planes*self.mul, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes*self.mul)
+        
+        self.shortcut = nn.Sequential()
+        
+        if stride != 1 or in_planes != out_planes*self.mul:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes*self.mul, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes*self.mul)
+            )
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+        
+
+    
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        #RGB 3개채널에서 64개의 Kernel 사용
+        self.in_planes = 64
+        
+        # Resnet 논문 구조 그대로 구현
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding = 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.linear = nn.Linear(512 * block.mul, num_classes)
+        
+    def make_layer(self, block, out_planes, num_blocks, stride):
+        # layer 앞부분에서만 크기를 절반으로 줄이므로, 아래와 같은 구조
+        strides = [stride] + [1] * (num_blocks-1)
+        layers = []
+        for i in range(num_blocks):
+            layers.append(block(self.in_planes, out_planes, strides[i]))
+            self.in_planes = block.mul * out_planes
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out,1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+def ResNet50():
+    return ResNet(BottleNeck, [3, 4, 6, 3])
+
+def ResNet101():
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+def ResNet152():
+    return ResNet(BottleNeck, [3, 8, 36, 3])
